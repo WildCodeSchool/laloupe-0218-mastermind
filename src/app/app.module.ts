5d1b9204ece7e5f3f01afe200ca262ec9685f501
AurelieAvisse <aurel.ly28000@gmail.com>
2018-03-27 17:10:08 +0200

    initial_commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,38 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 
+
+import { AngularFireModule } from 'angularfire2';
+
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
+import { Observable } from 'rxjs/Observable';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import 'hammerjs';
+import { Test1Component } from './test1/test1.component';
+
+import { routes } from './app.routes';
+import { LoginComponent } from './login/login.component';
+import { RulesComponent } from './rules/rules.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Test1Component,
+    LoginComponent,
+    RulesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    BrowserAnimationsModule,
+    routes
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCZCM3Msbku_lHRVk-OERLntC1kn0oVSYY",
+    authDomain: "books-f81ee.firebaseapp.com",
+    databaseURL: "https://books-f81ee.firebaseio.com",
+    projectId: "books-f81ee",
+    storageBucket: "books-f81ee.appspot.com",
+    messagingSenderId: "1051643817734"
+  }
 };
