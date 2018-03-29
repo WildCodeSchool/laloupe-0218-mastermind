fa08d4fc5ebbb3b20440a1fd2e036e6021809db5
AlexandreCoin <alexandre.pc.coin@gmail.com>
2018-03-29 11:50:05 +0200

    PR1 fixes update1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCZCM3Msbku_lHRVk-OERLntC1kn0oVSYY',
+    authDomain: 'books-f81ee.firebaseapp.com',
+    databaseURL: 'https://books-f81ee.firebaseio.com',
+    projectId: 'books-f81ee',
+    storageBucket: 'books-f81ee.appspot.com',
+    messagingSenderId: '1051643817734'
+  }
 };
